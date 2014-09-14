d9d01b98be26d7f056d8e6c2f3d5467bfd8b4370
oncletom <hi@oncletom.io>
2014-09-15 00:19:59 +0100

    withCredentials

--- src/configure.js
+++ src/configure.js
@@ -9,6 +9,7 @@ module.exports = function(app){
   app.use(logger('combined'));
   app.use(favicon(__dirname + '/../favicon.ico'));
   app.use(cors({
+    credentials: true,
     origin: function(origin, done){
       done(null, ['http://local.emunova.net:3000', 'http://emunova.net'].indexOf(origin) !== -1);
     }
