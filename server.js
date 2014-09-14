5e6f63fc0c7fa63e2003d0c2d7ea8246b89b5e82
oncletom <hi@oncletom.io>
2014-09-14 23:57:47 +0100

    favicon.ico

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 
 var express = require('express');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 var app = express();
 
 app.use(logger('combined'));
+app.use(favicon(__dirname + '/favicon.ico'));
 
 app.get('/', function(req, res){
   res.status(404).json({});
