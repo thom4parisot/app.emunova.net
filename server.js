fb8f261ae1ef136e20e5b98b367065d643d268f6
oncletom <hi@oncletom.io>
2014-09-14 23:53:31 +0100

    Logger and app skeletton.

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 'use strict';
+
+var express = require('express');
+var logger = require('morgan');
+var app = express();
+
+app.use(logger('combined'));
+
+app.get('/', function(req, res){
+  res.status(404).json({});
+});
+
+app.listen(process.env.PORT || 3000);
