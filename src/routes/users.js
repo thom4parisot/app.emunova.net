b49c302b77e31750e0e9628593c4c55b47d974bf
Thomas Parisot <thomas.parisot@bbc.co.uk>
2014-09-15 14:19:04 +0100

    Query and exports user data like SEO Name, profile URL and user name.

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var routes = require('express').Router();
 var cookieParser = require('cookie-parser');
 
 var cookieOptions = {
@@ -8,16 +7,44 @@ var cookieOptions = {
   domain: '.emunova.net'
 };
 
-routes.get('/me', cookieParser(null, cookieOptions), function(req, res){
-  if (!req.cookies.member_id) {
-    return res.status(404).send({});
-  }
+module.exports = function(app){
+  var routes = require('express').Router();
 
-  res.json({
-    user: {
-      id: req.cookies.member_id
+  routes.get('/me', cookieParser(null, cookieOptions), function(req, res){
+    if (!req.cookies.member_id) {
+      return res.status(404).send({});
+    }
+
+    app.locals.db
+      .select('name', 'members_seo_name')
+      .where('member_id', req.cookies.member_id)
+      .from('ibf_members')
+      .limit(1)
+      .first()
+      .then(successResponse, errorResponse);
+
+    //
+    function successResponse(row){
+      res.setHeader('Cache-Control', 'private');
+      res.setHeader('Expires', new Date(Date.now() + 3600000));
+
+      res.json({
+        user: {
+          id: Number(req.cookies.member_id),
+          name: row.name,
+          slug: row.members_seo_name,
+          profile_url: 'http://forums.emunova.net/user/' + req.cookies.member_id + '-' + row.members_seo_name + '/'
+        }
+      });
+    }
+
+    function errorResponse(err){
+      console.error('Failed to fetch user informations.');
+      console.error(err);
+
+      res.status(400).json({});
     }
   });
-});
 
-module.exports = routes;
+  return routes;
+};
